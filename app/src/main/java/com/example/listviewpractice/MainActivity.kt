faab2b4ff5e4100efd363acb2bf0ff3b5e8578fe
ichbong94 <ichbong94@gmail.com>
2022-05-31 19:22:23 +0900

    long

--- app/src/main/java/com/example/listviewpractice/MainActivity.kt
+++ app/src/main/java/com/example/listviewpractice/MainActivity.kt
@@ -2,10 +2,43 @@ package com.example.listviewpractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.listviewpractice.adapters.StudentAdapter
+import com.example.listviewpractice.datas.Student
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val mStudentList = ArrayList<Student>()
+    lateinit var mAdapter: StudentAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mStudentList.add(Student("이희봉", 1965))
+        mStudentList.add(Student("이희봉2", 1975))
+        mStudentList.add(Student("이희봉3", 1985))
+        mStudentList.add(Student("이희봉4", 1985))
+        mStudentList.add(Student("이희봉5", 1925))
+
+        mAdapter = StudentAdapter(this, R.layout.student_list_item, mStudentList)
+        studentListView.adapter = mAdapter
+
+        studentListView.setOnItemClickListener { parent, view, position, id ->
+
+            val clickedStudent = mStudentList[position]
+
+            Toast.makeText(this, "${clickedStudent.name}", Toast.LENGTH_SHORT).show()
+
+        }
+
+        studentListView.setOnItemLongClickListener { parent, view, position, id ->
+
+            val longClickedStudent = mStudentList[position]
+            Toast.makeText(this, "${longClickedStudent.name}길게", Toast.LENGTH_SHORT).show()
+
+            return@setOnItemLongClickListener true
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/listviewpractice/adapters/StudentAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.listviewpractice.adapters
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.example.listviewpractice.R
+import com.example.listviewpractice.datas.Student
+
+class StudentAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<Student>
+) : ArrayAdapter<Student>(mContext, resId, mList) {
+
+    val inf = LayoutInflater.from(mContext)
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+        if(tempRow == null){
+            tempRow = inf.inflate(R.layout.student_list_item, null)
+        }
+
+        val row = tempRow!!
+
+        val studentData = mList[position]
+
+        val nameTxt = row.findViewById<TextView>(R.id.nameTxt)
+        val birthYearTxt = row.findViewById<TextView>(R.id.birthYearTxt)
+
+        nameTxt.text = studentData.name
+        birthYearTxt.text = studentData.birthYear.toString()
+
+
+
+        return row
+    }
+
+
+}
